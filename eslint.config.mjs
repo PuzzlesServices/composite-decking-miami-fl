079789074000e54d4c6538341dcdcf593c5759be
Puzzles Consulting <services@puzzles.consulting>
2026-09-01 11:08:33 -0500

    (chore): Config files fixed and dependencies update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import globals from 'globals';
 
-export default [
-  ...eslintPluginAstro.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs['flat/recommended'],
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     rules: {
-      // Reglas personalizadas si se requieren
+      // Reglas personalizadas
     },
   },
-];
+);
